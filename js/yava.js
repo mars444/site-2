1d293a98d7dc1f0820392670fbe4771bbc270c41
mars444 <petromars444@gmail.com>
2021-07-23 21:42:55 +0300

    add site-2

---
+++ js/yava.js
@@ -0,0 +1,16 @@
+$(function () {
+	$('.tail__slider').slick({
+		nextArrow: '<button type="button" class="slick_button slick_next"><img src="img/next.svg" alt=""></button>' ,
+		prevArrow: '<button type="button" class="slick_button slick_prev"><img src="img/back.svg" alt=""></button>' ,
+		autoplay:true ,
+		autoplayspeed:3000,
+		fade:true
+	});
+	$('.trip__slider').slick({
+		nextArrow: '<button type="button" class="slick_button slick_next"><img src="img/next.svg" alt=""></button>' ,
+		prevArrow: '<button type="button" class="slick_button slick_prev"><img src="img/back.svg" alt=""></button>' ,
+		autoplay:true ,
+		autoplayspeed:3000,
+		fade:true
+	});
+    });
